e3d9ce5be2549c91035c4554fa370428e73b11f4
Maxim Koshman <maksimkoshman090@gmail.com>
2019-12-24 15:15:58 +0300

    add runing string

--- js/_custom.js
+++ js/_custom.js
@@ -1,71 +1,73 @@
 $(function() {
-
-    // let header = $('#header'),
-    //     statH = $("#stat").innerHeight(),
-    //     quoteH = $("#quote").innerHeight(),
-    //     footer__connectionH = $("#footer__connection").innerHeight(),
-    //     scrollOffset = $(window).scrollTop();
-
-    //     checkScroll(scrollOffset);
-
-    //     $(window).on('scroll', function () {
-    
-    //         scrollOffset = $(this).scrollTop();
-    //         checkScroll(scrollOffset);
-    
-    //       });
-    
-    
-    //     function checkScroll(scrollOffset) {
-    
-    
-    //         if (scrollOffset >= quoteH && scrollOffset < footer__connectionH) {
-            
-    //         }
-    //     }
-
-
-    let header = $('#header'),
+  let header = $("#header"),
     introH = $("#intro").innerHeight(),
     quoteH = $("#quote").innerHeight(),
+    statH = $("#stat").innerHeight(),
     scrollOffset = $(window).scrollTop();
 
-/* Fixed Header */
+  /* Fixed Header */
 
-checkScroll(scrollOffset);
+  checkScroll(scrollOffset);
 
-$(window).on('scroll', function () {
-
+  $(window).on("scroll", function() {
     scrollOffset = $(this).scrollTop();
     checkScroll(scrollOffset);
 
+    if (scrollOffset >= statH) {
+      $(".stat__number").each(function() {
+        $(this)
+          .prop("Counter", 0)
+          .animate(
+            {
+              Counter: $(this).text()
+            },
+            {
+              duration: 1000,
+              easing: "swing",
+              step: function(now) {
+                $(this).text(Math.ceil(now));
+              }
+            }
+          );
+      });
+    }
   });
 
-
-function checkScroll(scrollOffset) {
-
-
+  function checkScroll(scrollOffset) {
     if (scrollOffset >= introH) {
-        header.addClass("fixed");
+      header.addClass("fixed");
     } else {
-        header.removeClass("fixed");
+      header.removeClass("fixed");
     }
+  }
 
-    if (scrollOffset >= quoteH) {
-
-        $('.stat__number').each(function () {
-            $(this).prop('Counter', 0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 4000,
-                easing: 'swing',
-                step: function(now){
-                    $(this).text(Math.ceil(now));
-                }
-            });
-        });
-
-    }
+{
+    let el = document.getElementById('text_1');
+    let text = el.innerHTML;
+  
+    setInterval(function () {
+        text = text[text.length - 1] + text.substring(0, text.length - 1);
+        el.innerHTML = text;
+    }, 500)
 }
-        
+
+{
+    let el = document.getElementById('text_2');
+    let text = el.innerHTML;
+  
+    setInterval(function () {
+        text = text[text.length - 1] + text.substring(0, text.length - 1);
+        el.innerHTML = text;
+    }, 300)
+}
+
+{
+    let el = document.getElementById('text_3');
+    let text = el.innerHTML;
+  
+    setInterval(function () {
+        text = text[text.length - 1] + text.substring(0, text.length - 1);
+        el.innerHTML = text;
+    }, 400)
+}
 });
